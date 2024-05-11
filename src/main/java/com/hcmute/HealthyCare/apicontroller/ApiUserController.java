7772ead1a7163028cc01e34c8db7fbc9754452f9
MN <57828955+MNgaminhhh@users.noreply.github.com>
2024-05-12 00:39:17 +0700

    update view doctor person

--- src/main/java/com/hcmute/HealthyCare/apicontroller/ApiUserController.java
+++ src/main/java/com/hcmute/HealthyCare/apicontroller/ApiUserController.java
@@ -14,12 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import com.hcmute.HealthyCare.entity.Account;
@@ -62,7 +57,16 @@ public class ApiUserController {
         } else {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-    } 
+    }
+    @GetMapping("/user/{email}")
+    public ResponseEntity<?> getUserByEmail(@PathVariable String email) {
+        User user = userService.findUserByEmail(email);
+        if (user != null) {
+            return ResponseEntity.ok().body(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
     @GetMapping("/info")
     public ResponseEntity<?> getUserInfo() {
